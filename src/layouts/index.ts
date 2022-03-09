dd6c446e6f57b84fcab24059389eec2d21805741
Buliqioqiolibusdo <dingyeran@163.com>
2022-03-10 02:44:30 +0800

    by zhizhong

--- src/layouts/index.ts
+++ src/layouts/index.ts
@@ -1,11 +1,11 @@
-import BlankLayout from '.\BlankLayout.vue';
-import Header from '.\components\Header.vue';
-import Sidebar from '.\components\Sidebar.vue';
-import TabsView from '.\components\TabsView.vue';
-import DetailLayout from '.\content\detail\DetailLayout.vue';
-import ListLayout from '.\content\list\ListLayout.vue';
-import SimpleLayout from '.\content\simple\SimpleLayout.vue';
-import NormalLayout from '.\NormalLayout.vue';
+import BlankLayout from './BlankLayout.vue';
+import Header from './components/Header.vue';
+import Sidebar from './components/Sidebar.vue';
+import TabsView from './components/TabsView.vue';
+import DetailLayout from './content/detail/DetailLayout.vue';
+import ListLayout from './content/list/ListLayout.vue';
+import SimpleLayout from './content/simple/SimpleLayout.vue';
+import NormalLayout from './NormalLayout.vue';
 
 export {
   BlankLayout as ClBlankLayout,
